fe032aeb068c306b8436581b11aff0c749b0cf84
KSY <sykim.p508@gmail.com>
2023-06-26 13:32:08 +0000

    Display lists

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -2,12 +2,15 @@ import { fail } from '@sveltejs/kit';
 import { superValidate } from 'sveltekit-superforms/server';
 
 import prisma from '$lib/server/prisma';
+import { appRouter } from '$lib/server/router';
+import { createContext } from '$lib/server/trpc';
 import { taskSchema } from '$lib/zod';
 
 import type { Actions, PageServerLoad } from './$types';
 
 export const load = (async () => ({
-	form: await superValidate(taskSchema)
+	form: await superValidate(taskSchema),
+	tasks: appRouter.createCaller(await createContext()).inbox.getInbox()
 })) satisfies PageServerLoad;
 
 export const actions = {
