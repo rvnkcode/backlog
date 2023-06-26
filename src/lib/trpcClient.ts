d9d3ea8e83b9acc9c737da8521d4e56ebb72ea9d
KSY <sykim.p508@gmail.com>
2023-06-26 12:12:06 +0000

    Add trpc configurations

---
+++ src/lib/trpcClient.ts
@@ -0,0 +1,15 @@
+import { createTRPCProxyClient, httpBatchLink } from '@trpc/client';
+import superjson from 'superjson';
+
+import type { AppRouter } from './server/router';
+
+export const trpcApiBase = '/api/trpc';
+
+export const trpc = createTRPCProxyClient<AppRouter>({
+	links: [
+		httpBatchLink({
+			url: trpcApiBase
+		})
+	],
+	transformer: superjson
+});
---
+++ src/routes/api/trpc/inbox.ts
@@ -0,0 +1,15 @@
+import { publicProcedure } from '$lib/server/trpc';
+import { router } from '$lib/server/trpc';
+
+export const inboxRouter = router({
+	getInbox: publicProcedure.query(async ({ ctx }) => {
+		return await ctx.prisma.task.findMany({
+			select: {
+				id: true,
+				title: true,
+				isStarted: true,
+				isDone: true
+			}
+		});
+	})
+});
