295da9c762c950bc2917a39771a19735ebc870b7
KSY <sykim.p508@gmail.com>
2023-06-27 12:35:40 +0000

    Update zod schema: trim white spaces

--- src/lib/zod.ts
+++ src/lib/zod.ts
@@ -2,7 +2,7 @@ import type { Prisma } from '@prisma/client';
 import { z } from 'zod';
 
 export const taskSchema = z.object({
-	title: z.string().min(1)
+	title: z.string().trim().min(1)
 }) satisfies z.ZodType<Prisma.TaskCreateInput>;
 
 export type TaskSchema = typeof taskSchema;
