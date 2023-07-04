6c6eb62d75008fbe70e084d6d003f0ad7444311f
KSY <sykim.p508@gmail.com>
2023-07-04 13:15:09 +0000

    Remove unnecessary test and add TODO comments

--- tests/e2e.test.ts
+++ tests/e2e.test.ts
@@ -5,21 +5,7 @@ test.beforeEach(async ({ page }) => {
 	await page.goto('/');
 });
 
-test('should have page title', async ({ page }) => {
-	await expect(page).toHaveTitle(/Inbox/);
-});
-
-test(`should display elements`, async ({ page }) => {
-	await expect(page.getByRole('heading', { name: 'Backlog', level: 1 })).toBeVisible();
-	await expect(page.getByRole('heading', { name: 'Inbox', level: 2 })).toBeVisible();
-	await expect(page.getByPlaceholder('New To-Do')).toBeVisible();
-	await expect(page.getByRole('button', { name: 'Add' })).toBeVisible();
-	await expect(page.getByRole('list')).toBeVisible();
-	await expect(page.locator('ul > li > span')).toHaveText(['This is the sample task']);
-	await expect(page.locator('ul > li > input')).toHaveAttribute('type', 'checkbox');
-});
-
-test(`should create a task with enter key`, async ({ page }) => {
+test(`should create a task`, async ({ page }) => {
 	const input = page.getByPlaceholder('New To-Do');
 	const mockedTask = `test`;
 
@@ -29,23 +15,12 @@ test(`should create a task with enter key`, async ({ page }) => {
 	await expect(page.locator('ul > li')).toHaveText([`This is the sample task`, mockedTask]);
 
 	const checkbox = page.getByLabel(mockedTask);
-	await expect(checkbox).toBeAttached();
 	await expect(checkbox).toHaveAttribute('type', 'checkbox');
-	await expect(checkbox).toBeEditable();
+	expect(await checkbox.isChecked()).toBeFalsy();
 });
 
-test(`should create a task with submit button`, async ({ page }) => {
-	const input = page.getByPlaceholder(`New To-Do`);
-	const submitButton = page.getByRole(`button`, { name: `Add` });
-	const mockedTask = `test2`;
-
-	await input.fill(mockedTask);
-	await submitButton.click();
-	await expect(input).toBeEmpty();
-	await expect(page.locator('ul > li')).toHaveText([`This is the sample task`, `test`, mockedTask]);
-
-	const checkbox = page.getByLabel(mockedTask);
-	await expect(checkbox).toBeAttached();
-	await expect(checkbox).toHaveAttribute('type', 'checkbox');
-	await expect(checkbox).toBeEditable();
-});
+// TODO:
+// update task's status
+// `go to the detail page`
+// `should return 403 if id isn't exists`
+// `update task's title`
