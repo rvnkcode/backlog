28af54d41811c09749e70866d9b13fdfb096a22a
KSY <sykim.p508@gmail.com>
2023-07-05 12:13:07 +0000

    Update e2e test:
    - go to the detail page of the task
    - update the task
    - display updated task
    - display 403 page

--- tests/e2e.test.ts
+++ tests/e2e.test.ts
@@ -1,26 +1,82 @@
 // Run $ yarn prisma migrate reset before running e2e tests
 import { expect, test } from '@playwright/test';
 
+// Global const
+const tasks = [`This is the sample task`, `test`];
+const updateValue = `Completely different value`;
+
 test.beforeEach(async ({ page }) => {
 	await page.goto('/');
 });
 
-test(`should create a task`, async ({ page }) => {
-	const input = page.getByPlaceholder('New To-Do');
-	const mockedTask = `test`;
+test(`should have a title`, async ({ page }) => {
+	await expect(page).toHaveTitle('Backlog: Inbox');
+});
 
-	await input.fill(mockedTask);
-	await input.press('Enter');
-	await expect(input).toBeEmpty();
-	await expect(page.locator('ul > li')).toHaveText([`This is the sample task`, mockedTask]);
+test(`should display some elements`, async ({ page }) => {
+	await expect(page.getByRole('heading', { name: 'Backlog', level: 1 })).toBeVisible();
+	await expect(page.getByRole('heading', { name: 'Inbox', level: 2 })).toBeVisible();
 
-	const checkbox = page.getByLabel(mockedTask);
+	await expect(page.getByPlaceholder('New To-Do')).toBeVisible();
+	await expect(page.getByRole('button', { name: 'Add' })).toBeVisible();
+
+	await expect(page.getByRole('list')).toBeVisible();
+	await expect(page.locator('ul > li > label > a')).toHaveText(tasks[0]);
+
+	const checkbox = page.getByLabel(tasks[0]);
 	await expect(checkbox).toHaveAttribute('type', 'checkbox');
 	expect(await checkbox.isChecked()).toBeFalsy();
 });
 
+test(`should create a task`, async ({ page }) => {
+	const input = page.getByPlaceholder('New To-Do');
+
+	await input.fill(tasks[1]);
+	await input.press('Enter');
+	await expect(input).toBeEmpty();
+	await expect(page.locator('ul > li')).toHaveText(tasks);
+
+	const checkbox = page.getByLabel(tasks[1]);
+	await expect(checkbox).toHaveAttribute('type', 'checkbox');
+	expect(await checkbox.isChecked()).toBeFalsy();
+});
+
+test(`should go to the task detail page`, async ({ page }) => {
+	const links = page.getByRole('link');
+
+	await links.nth(0).click();
+	await expect(page).toHaveURL(/.task\/1/);
+	await expect(page.getByRole('heading', { name: 'Backlog', level: 1 })).toBeVisible();
+	expect(await page.getByRole('textbox').inputValue()).toStrictEqual(tasks[0]);
+	await expect(page.getByRole('button', { name: 'Update' })).toBeVisible();
+});
+
+test(`should update task's title`, async ({ page }) => {
+	const link = page.getByRole('link', { name: tasks[1] });
+	await link.click();
+	await expect(page).toHaveURL(/.task\/2/);
+
+	const input = page.getByRole('textbox');
+	await input.fill(updateValue);
+	await input.press(`Enter`);
+	expect(await input.inputValue()).toStrictEqual(updateValue);
+
+	await page.goBack();
+	await expect(page.locator('ul > li')).toHaveText([tasks[0], updateValue]);
+});
+
+test(`should display updated list`, async ({ page }) => {
+	await expect(page.locator('ul > li')).toHaveText([tasks[0], updateValue]);
+});
+
+test(`should display 403 error page`, async ({ page }) => {
+	await page.goto('/task/99');
+	await expect(page.getByRole('heading', { name: 'Backlog', level: 1 })).toBeVisible();
+	await expect(page.getByRole('textbox')).not.toBeVisible();
+	await expect(page.getByRole('button')).not.toBeVisible();
+	await expect(page.getByText('403')).toBeVisible();
+	await expect(page.getByText('Not found')).toBeVisible();
+});
+
 // TODO:
 // update task's status
-// `go to the detail page`
-// `should return 403 if id isn't exists`
-// `update task's title`
