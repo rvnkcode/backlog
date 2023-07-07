65b36b5939c0796f8af8b6f47ec948a0c0749c76
KSY <sykim.p508@gmail.com>
2023-07-07 11:45:16 +0000

    Make list of task titles to the const for using seed and test files

---
+++ src/lib/const.ts
@@ -0,0 +1,8 @@
+export const inboxTitles = [
+	`This is the sample task`,
+	`Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.`,
+	`It's completed task`,
+	`test`
+];
+
+export const trashTitles = [`It's trashed task`];
