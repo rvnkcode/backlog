859fcc2b9f9891b81fea57f9a16b3d128cfe84e1
KSY <sykim.p508@gmail.com>
2023-07-11 12:57:03 +0000

    Add some styles to the form
    - Make button icons more larger
    - Set input focus bg color
    - Remove focus outline

--- src/lib/components/Form.svelte
+++ src/lib/components/Form.svelte
@@ -25,25 +25,34 @@
 			placeholder="New To-Do"
 			required
 			bind:value={$form.title}
-			class="grow"
+			class="grow p-1 focus:bg-gray-100 focus:outline-none"
 		/>
 		{#if !isEdit}
-			<button type="button" on:click={() => (showMore = !showMore)} data-testid="showMoreButton"
-				><ion-icon name="ellipsis-vertical" /></button
+			<button
+				type="button"
+				on:click={() => (showMore = !showMore)}
+				data-testid="showMoreButton"
+				class="px-3"><ion-icon name="ellipsis-vertical" /></button
 			>
 		{/if}
-		<button type="submit">
-			<ion-icon name="add" />
+		<button type="submit" class="px-3 bg-black">
+			<ion-icon name="add" class="text-white" />
 		</button>
 	</div>
 	{#if showMore || isEdit}
-		<textarea name="note" placeholder="Notes" bind:value={$form.note} class="w-full" rows="4" />
+		<textarea
+			name="note"
+			placeholder="Notes"
+			bind:value={$form.note}
+			class="w-full p-1 mt-1 focus:bg-gray-100 focus:outline-none"
+			rows="4"
+		/>
 	{/if}
 </form>
 
 <!-- #region Debug -->
 
-<SuperDebug data={$form} />
+<!-- <SuperDebug data={$form} /> -->
 
 {#if $allErrors.length}
 	<ul>
