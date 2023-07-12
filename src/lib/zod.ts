4fca02a375318b62490c0e8c246124b6795f6617
KSY <sykim.p508@gmail.com>
2023-07-12 13:14:22 +0000

    Add urls to the zod schema

--- src/lib/zod.ts
+++ src/lib/zod.ts
@@ -6,7 +6,8 @@ export const idSchema = z.number().positive();
 export const taskSchema = z.object({
 	id: z.number().positive().optional(),
 	title: z.string().trim().min(1),
-	note: z.string().trim().nullish()
+	note: z.string().trim().nullish(),
+	urls: z.string().trim().nullish()
 }) satisfies z.ZodType<Prisma.TaskCreateInput>;
 
 export type TaskSchema = typeof taskSchema;
