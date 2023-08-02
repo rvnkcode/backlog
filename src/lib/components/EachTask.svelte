4e36b100239234221fdc19fa57a40806133792f2
KSY <sykim.p508@gmail.com>
2023-08-02 02:26:08 +0000

    Separate task icons component

--- src/lib/components/EachTask.svelte
+++ src/lib/components/EachTask.svelte
@@ -1,8 +1,8 @@
 <script lang="ts">
-	import { tooltip } from '$lib/actions/tooltip';
 	import type { RouterOutput } from '$lib/server/router';
 	import TaskCheckbox from './atoms/TaskCheckbox.svelte';
 	import TaskDeleteButton from './atoms/TaskDeleteButton.svelte';
+	import TaskInfoIcons from './atoms/TaskInfoIcons.svelte';
 	import TaskTitle from './atoms/TaskTitle.svelte';
 
 	export let task: RouterOutput['inbox']['getInbox'][number];
@@ -15,19 +15,7 @@
 		<TaskCheckbox id={task.id} isDone={task.isDone} {indeterminate} />
 		<label class="flex" for={task.id.toString()}>
 			<TaskTitle id={task.id} title={task.title} />
-			<div class="ml-2">
-				{#if task.note}
-					<ion-icon name="document-outline" class="text-sm" use:tooltip tooltipText={task.note} />
-				{/if}
-
-				{#if task.urls?.length}
-					{#each task.urls as url}
-						<a href={url} target="_blank">
-							<ion-icon name="link" class="text-sm text-sky-500" use:tooltip tooltipText={url} />
-						</a>
-					{/each}
-				{/if}
-			</div>
+			<TaskInfoIcons note={task.note} urls={task.urls} />
 		</label>
 	</div>
 
---
+++ src/lib/components/atoms/TaskInfoIcons.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import { tooltip } from '$lib/actions/tooltip';
+
+	export let note: string | undefined;
+	export let urls: string[] | undefined;
+</script>
+
+<div class="ml-2">
+	{#if note}
+		<ion-icon name="document-outline" class="text-sm" use:tooltip tooltipText={note} />
+	{/if}
+
+	{#if urls?.length}
+		{#each urls as url}
+			<a href={url} target="_blank">
+				<ion-icon name="link" class="text-sm text-sky-500" use:tooltip tooltipText={url} />
+			</a>
+		{/each}
+	{/if}
+</div>
