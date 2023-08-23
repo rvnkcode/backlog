012e8ea6d119a8d26bfd7e4038ac607da7ed5344
KSY <sykim.p508@gmail.com>
2023-08-23 20:15:53 +0900

    Display allocated contact's name

--- src/lib/components/EachTask.svelte
+++ src/lib/components/EachTask.svelte
@@ -5,17 +5,21 @@
   import TaskDeleteButton from './atoms/TaskDeleteButton.svelte';
   import TaskTitle from './atoms/TaskTitle.svelte';
   import TaskInfoIcons from './molecules/TaskInfoIcons.svelte';
+  import { page } from '$app/stores'
 
   export let task: RouterOutput['list']['getInbox'][number];
 
   $: indeterminate = task.isStarted && !task.isDone ? true : false;
+  $: current = $page.url.pathname
 </script>
 
 <li class="group flex justify-between">
   <div class="flex items-center">
     <TaskCheckbox id={task.id} isDone={task.isDone} {indeterminate} />
-    <!-- TODO: Display allocated contact's name -->
     <label for={task.id.toString()}>
+      {#if task.allocatedTo && current === "/waiting_for"}
+        <span class="mr-1 text-blue-600">{task.allocatedTo}</span>
+      {/if}
       <TaskTitle id={task.id} title={task.title} isDone={task.isDone} />
       <TaskInfoIcons note={task.note} urls={task.urls} />
     </label>
