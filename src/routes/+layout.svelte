5f6732f7209854f9e0b0459e194927cc7718b45a
KSY <sykim.p508@gmail.com>
2023-08-23 21:02:02 +0900

    FIX: Add aria-label to the hamburger menu button and icons

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -15,12 +15,12 @@
 
 <header class="px-2">
   <h1 class="font-semibold text-lg">
-    <button on:click={() => (showMenu = !showMenu)}>
+    <button on:click={() => (showMenu = !showMenu)} aria-label="Toggle hamburger menu button">
       <!-- TODO: transition -->
       {#if showMenu}
-        <ion-icon name="close-outline" />
+        <ion-icon name="close-outline" aria-label="Close side menu icon" />
       {:else}
-        <ion-icon name="menu-outline" />
+        <ion-icon name="menu-outline" aria-label="Open side menu icon" />
       {/if}
     </button>
 
