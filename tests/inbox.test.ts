ab76a46bbd24d2edb9efc76297e7d8be976742f1
KSY <sykim.p508@gmail.com>
2023-08-29 20:31:18 +0900

    yarn format

--- tests/inbox.test.ts
+++ tests/inbox.test.ts
@@ -30,7 +30,7 @@ test('should display some elements', async ({ page }) => {
 test('should go to the settings page', async ({ page }) => {
   await page.getByRole('link', { name: 'Settings icon Settings' }).click();
   await expect(page).toHaveURL('settings');
-})
+});
 
 test('should not have any automatically detectable accessibility issues', async ({ page }) => {
   const accessibilityScanResults = await new AxeBuilder({ page }).analyze();
