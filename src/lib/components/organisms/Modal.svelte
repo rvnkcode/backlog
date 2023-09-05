5509730941529b586a58e48673da3661095305ea
KSY <sykim.p508@gmail.com>
2023-09-05 17:23:16 +0900

    Create general modal component

---
+++ src/lib/components/organisms/Modal.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+  export let open: boolean;
+
+  const closeModal = (e: KeyboardEvent) => {
+    if (e.key === 'Escape') {
+      open = false;
+    } else return;
+  };
+</script>
+
+{#if open}
+  <div class="backdrop" />
+{/if}
+
+<dialog {open} class="p-4">
+  <slot />
+</dialog>
+
+<svelte:window on:keydown={closeModal} />
